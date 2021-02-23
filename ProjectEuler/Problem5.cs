78e49355c90e6678e635ae52d0da2c22c8c5ec6f
rafael3do <rafael3do@hotmail.com>
2021-02-22 21:19:24 -0300

    published sumary problem5

--- ProjectEuler/Problem5.cs
+++ ProjectEuler/Problem5.cs
@@ -3,6 +3,10 @@
 
 namespace ProjectEuler
 {
+    ///</sumary>
+    ///2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    ///What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+    ///</sumary>
     class Problem5
     {
         static int lcm(int a, int b)
