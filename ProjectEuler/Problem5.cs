d8af91f325208d2e13c1f612fc65545a2cc37532
rafael3do <rafael3do@hotmail.com>
2021-02-22 21:03:59 -0300

    Solved Problem 5

---
+++ ProjectEuler/Problem5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace ProjectEuler
+{
+    class Problem5
+    {
+        static int lcm(int a, int b)
+        {
+            return a / gcd(a, b) * b;
+        }
+        static void Main(string[] args)
+        {
+            int star = 1;
+            for (int i = 2; i <= 20; i++)
+            {
+                star = lcm(star, i);
+            }
+            Console.WriteLine(star);
+            Console.ReadKey();
+        }
+        
+        static int gcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                a %= b;
+                a ^= b;
+                b ^= a;
+                a ^= b;
+            }
+            return a;
+        }
+    }
+}
