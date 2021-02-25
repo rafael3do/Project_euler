6955bee20b0488aeb70305a6f92ee82fd6e26b78
rafael3do <rafael3do@hotmail.com>
2021-02-24 22:03:20 -0300

    Solved Problem 7

---
+++ ProjectEuler/Problem7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    class Problem7
+    {
+        static void Main (string[] args)
+        {
+            List<long> numbers = new List<long>() {2};
+
+            for (long i = 3; i < long.MaxValue; i += 2)
+            {
+                if (!numbers.Any(p => (i % p) == 0))
+                {
+                    numbers.Add(i);
+                    if (numbers.Count == 10001)
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
