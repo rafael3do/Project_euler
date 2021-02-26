35328fe5013be20d1bbd75ede3ca34d94e331eb2
rafael3do <rafael3do@hotmail.com>
2021-02-26 20:39:34 -0300

    Solved Problem 9

---
+++ ProjectEuler/Problem9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    /// <summary>
+    /// Um tripleto pitagórico é um conjunto de três números naturais, a < b < c , para os quais,
+    /// Por exemplo, 3 2 + 4 2 = 9 + 16 = 25 = 5 2 .
+    /// Existe exatamente um tripleto pitagórico para o qual a + b + c = 1000.
+    /// Encontre o produto abc .
+    /// </summary>
+    class Problem9
+    {
+        static void Main (string [] args)
+        {
+            int sum = 1000;
+            for (int i = 0; i <= sum; i++)
+            {
+                for (int j =i+1;j<=sum;j++)
+                {
+                    for (int k = j + 1; k <= sum; k++)
+                    {                       
+                        if ((i * i) + (j * j) == (k * k) && ((i + j + k == sum)))
+                        {
+                            Console.WriteLine(i + " " + j + " " + k);
+                            Console.WriteLine(i * j * k);
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
