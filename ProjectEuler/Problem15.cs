77f5483a3f419fb6f0054fa86d22eda5649e53d9
Rafael Figueiredo <rafael3do@hotmail.com>
2021-03-11 21:02:18 -0300

    Solved problem 15

---
+++ ProjectEuler/Problem15.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace ProjectEuler
+{
+    class Problem15
+    {
+        /// <summary>
+        /// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+        /// How many such routes are there through a 20×20 grid?
+        /// </summary>
+
+        static void Main (string [] args)
+        {
+            const int gridSize = 20;
+            long paths = 1;
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                paths *= (2 * gridSize) - i;
+                paths /= i + 1;
+            }
+            Console.WriteLine(paths.ToString());
+            Console.ReadKey();
+        }
+    }
+}
