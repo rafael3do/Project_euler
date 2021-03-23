de9d9ed2b318ee6a7647d63d8fe92d1827bc56c3
Rafael Figueiredo <rafael3do@hotmail.com>
2021-03-23 19:49:13 -0300

    Solved Problem 22

---
+++ ProjectEuler/Problem22.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem22
+    {
+      /// <summary>
+      /// 
+      /// </summary>
+ 
+        static void Main (string[] args)
+        {
+            StreamReader sr = new StreamReader("names.txt");
+            string textNames = sr.ReadToEnd().Replace("/", "").Replace("\"", "");
+            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            long result = 0;
+            string[] arr = textNames.Split(',');
+            Array.Sort(arr);
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                int score = 0;
+                foreach (var eachChar in arr[i - 1].ToCharArray())
+                {
+                    score += (Array.IndexOf(alpha, eachChar) + 1);
+                }
+                score *= i;
+                result += score;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
