75aa7f0aed123be0764a29270a759d0113db2e9e
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-09 21:14:32 -0300

    Solved  problem 35

---
+++ ProjectEuler/Problem35.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem35
+    {
+        /// <summary>
+        /// The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+        ///There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+        ///How many circular primes are there below one million?
+        /// </summary>
+
+        public static SortedSet<int> primes;
+        static void Main(string[] args)
+        {
+
+            int noCircularPrimes = 2;
+            primes = new SortedSet<int>(ESieve(1000000));
+
+            primes.Remove(2);
+            primes.Remove(5);
+
+            while (primes.Count > 0)
+            {
+                noCircularPrimes += CheckCircularPrimes(primes.Min);
+            }
+
+            Console.WriteLine(noCircularPrimes);
+            Console.ReadKey();
+
+        }
+
+
+        static int CheckCircularPrimes(int prime)
+        {
+            int multiplier = 1;
+            int number = prime;
+            int count = 0;
+            int d;
+
+
+            while (number > 0)
+            {
+                d = number % 10;
+                if (d % 2 == 0 || d == 5)
+                {
+                    primes.Remove(prime);
+                    return 0;
+                }
+                number /= 10;
+                multiplier *= 10;
+                count++;
+            }
+            multiplier /= 10;
+
+
+            number = prime;
+            List<int> foundCircularPrimes = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                if (primes.Contains(number))
+                {
+                    foundCircularPrimes.Add(number);
+                    primes.Remove(number);
+                }
+                else if (!foundCircularPrimes.Contains(number))
+                {
+                    return 0;
+                }
+
+                d = number % 10;
+                number = d * multiplier + number / 10;
+            }
+
+            return foundCircularPrimes.Count;
+        }
+        static int[] ESieve(int upperLimit)
+        {
+
+            int sieveBound = (int)(upperLimit - 1) / 2;
+            int upperSqrt = ((int)Math.Sqrt(upperLimit) - 1) / 2;
+
+            BitArray PrimeBits = new BitArray(sieveBound + 1, true);
+
+            for (int i = 1; i <= upperSqrt; i++)
+            {
+                if (PrimeBits.Get(i))
+                {
+                    for (int j = i * 2 * (i + 1); j <= sieveBound; j += 2 * i + 1)
+                    {
+                        PrimeBits.Set(j, false);
+                    }
+                }
+            }
+
+            List<int> numbers = new List<int>((int)(upperLimit / (Math.Log(upperLimit) - 1.08366)));
+            numbers.Add(2);
+            for (int i = 1; i <= sieveBound; i++)
+            {
+                if (PrimeBits.Get(i))
+                {
+                    numbers.Add(2 * i + 1);
+                }
+            }
+
+            return numbers.ToArray();
+        }
+
+    }
+}
