cbc3ded22baafd383f86cf24ef02c2c5197f5108
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-19 21:24:02 -0300

    Solved Problem 40

--- ProjectEuler/Problem40.cs
+++ ProjectEuler/Problem40.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,30 +9,41 @@ namespace ProjectEuler
 {
     class Problem40
     {
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="args"></param>
-        static void Main (string [] args)
+        /// <summary>
+        /// An irrational decimal fraction is created by concatenating the positive integers:
+        ///0.123456789101112131415161718192021...
+        ///It can be seen that the 12th digit of the fractional part is 1.
+        ///If dn represents the nth digit of the fractional part, find the value of the following expression.
+        ///d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
         {
-			int prod = 1;
-			StringBuilder sb = new StringBuilder();
-			for (int i = 1; i < 1000000; i++)
-			{
-				sb.Append(i);
+            long result = 1;
+            List<long> counts = new List<long>() { 0 };
+            long last = 0;
+            for (long i = 0; last < 1000000; i++)
+            {
+                long count = (long)((i + 1) * 9 * (Math.Pow(10, (int)i)));
+                last += count;
+                counts.Add(last);
+            }
 
-				
-				for (int j = 1; j <= 6; j++)
-				{
-					//prod * = (i / (int)pow(10, digits - j - 1)) % 10; /* retrieve i's j-th digit */
-					prod *= (j / (int)Math.Pow(10, 6 - j - 1)) % 10;
-					//prod *= sb[(Math.Pow(10.0, j) - 1.0) - '0'];
-				}
-			}
-			Console.WriteLine(prod);
-			Console.ReadKey();
-		
-
-	}
+            for (int i = 0; i <= 6; i++)
+            {
+                long digitN = (long)(Math.Pow(10, i) - 1);
+                int n = counts.Count - 1;
+                while (counts[n] > digitN)
+                {
+                    n--;
+                }
+                long x = (long)(Math.Pow(10, n) + (digitN - counts[n]) / (n + 1));
+                long d = long.Parse(x.ToString()[(int)((digitN - counts[n]) % (n + 1))].ToString());
+                result *= d;
+                //Console.WriteLine("d{0} = {1}", digitN + 1, d);
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
     }
 }
